136710897322235b6c42f0ca07fc5c3ccad3d1ae
marcosbroc <mbroc@ontarget21.co.uk>
2025-08-09 23:47:52 +0200

    New commit test

--- Greeting/src/Greeting.sol
+++ Greeting/src/Greeting.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+contract Greeting {
+    string public greetingMessage;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    function getGreeting(
+        string memory _name
+    ) public view returns (string memory) {
+        if (bytes(_name).length > 0)
+            // The way to concatenate strings in Solidity
+            return string(abi.encodePacked(greetingMessage, " ", _name));
+        else return greetingMessage;
     }
 
-    function increment() public {
-        number++;
+    function setGreetingMessage(string memory _greetingMessage) public {
+        greetingMessage = _greetingMessage;
     }
 }
